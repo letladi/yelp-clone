0144e889509d9b4d7c42a4f43c1fe44d667b4196
Letladi <sletladi@gmail.com>
2016-09-01 01:08:31 +0200

    created a sessions controller to for users logging in and out

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,5 +2,5 @@ Fabricator(:user) do
 	first_name { Faker::Name.first_name }
 	last_name { Faker::Name.last_name }
 	email { Faker::Internet.email }
-	password { Faker::Internet.email }
+	password { Faker::Internet.password }
 end
