46eab25ba4439d60578c18157189d9ca62bc5db6
Letladi <sletladi@gmail.com>
2016-09-01 00:21:23 +0200

    hooked up user registration form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def input_error_class(model, property)
+		input_error_message(model, property).blank? ? '' : 'form-control-danger'
+	end
+
+	def input_error_message(model, property)
+		property_error = model.errors[property].uniq.join(' | ')
+		property_error.blank? ? '' : property_error
+	end
+
+	def container_error_class(model, property)
+		model.errors[property].empty? ? 'has-success' : 'has-danger'
+	end
 end
