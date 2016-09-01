697425f150eeb7d1ccd3b21f0b3c3363b832e080
Letladi <sletladi@gmail.com>
2016-09-01 12:37:54 +0200

    added categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,24 @@
 class CategoriesController < ApplicationController 
-	before_action :require_user
+	before_action :require_user, only: [:new, :create]
 	def new
 		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(category_params)
+
+		if @category.save
+			redirect_to root_path
+			flash[:success] = "new category was created successfully."
+		else
+			flash[:danger] = "Category was not created"
+			render :new
+		end
+	end
+
+	private
+
+	def category_params
+		params.require(:category).permit(:name)
 	end
 end
