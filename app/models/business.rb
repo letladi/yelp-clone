e3b21dfb357dadca355ca6e87ff520d14c0863dd
Letladi <sletladi@gmail.com>
2016-09-01 20:21:13 +0200

    added #review_count and #rating methods to business model

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,4 +3,17 @@ class Business < ApplicationRecord
 	has_many :reviews
 
 	validates_presence_of :name
+
+	def rating
+		if reviews.empty?
+			rating = 0.0
+		else
+			rating = reviews.map(&:rating).inject(&:+) / review_count.to_f
+		end
+		rating.round(1)
+	end
+
+	def review_count
+		reviews.size
+	end
 end
--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -4,4 +4,32 @@ describe Business do
 	it { should validate_presence_of(:name) }
 	it { should belong_to(:category) }
 	it { should have_many(:reviews) }
+
+	describe "#rating" do
+		it "returns the average for the all the reviews" do 
+			business = Fabricate(:business)
+			review1 = Fabricate(:review, rating: 4, business: business) 
+		  review2 = Fabricate(:review, rating: 2, business: business) 
+		  review3 = Fabricate(:review, rating: 2, business: business)
+			expect(business.rating).to eq(2.7)
+		end
+		it "returns zero if there are no reviews" do
+			business = Fabricate(:business)
+			expect(business.rating).to eq(0.0)
+		end
+	end
+
+	describe "#review_count" do
+		it "returns the number of reviews associated with a business" do
+			business = Fabricate(:business)
+			review1 = Fabricate(:review, rating: 4, business: business) 
+		  review2 = Fabricate(:review, rating: 2, business: business) 
+		  review3 = Fabricate(:review, rating: 2, business: business)
+		  expect(business.review_count).to eq(3)
+		end
+		it "returns 0 there are no reviews" do 
+			business = Fabricate(:business)
+			expect(business.review_count).to eq(0)
+		end
+	end
 end
