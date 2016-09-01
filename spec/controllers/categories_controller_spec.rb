2a5f08037fbfe2f52aebb377a1a34ada8af51261
Letladi <sletladi@gmail.com>
2016-09-01 12:37:54 +0200

    added categories controller

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -15,16 +15,37 @@ describe CategoriesController do
 		end
 	end 
 
-	describe 'POST create' do
-		before do 
-			post :create, params: { name: Fabricate(:category) }
-		end
-		it "redirects to the root path" do
-			
-		end
-		it "creates a new category"
+	describe 'POST create' do	
 		it_behaves_like "require_login" do
-			let(:action) { get :new }
+			let(:action) { post :create, category:  Fabricate.attributes_for(:category)  }
+		end
+		context "with valid input" do 
+			before do 
+				post :create, category:  Fabricate.attributes_for(:category) 
+			end
+			it "redirects to the root path" do
+				expect(response).to redirect_to root_path
+			end
+			it "creates a new category" do 
+				expect(Category.count).to eq(1)
+			end
+			it "sets success error message" do 
+				expect(flash[:success]).to be
+			end
+		end
+		context "with invalid inputs" do 
+			before do 
+				post :create, category:  Fabricate.attributes_for(:category, name: '') 
+			end
+			it "sets error message" do 
+				expect(flash[:danger]).to be
+			end
+			it "does not create a category" do 
+				expect(Category.count).to eq(0)
+			end
+			it "renders the new template" do 
+				expect(response).to render_template :new
+			end
 		end
 	end
 end
