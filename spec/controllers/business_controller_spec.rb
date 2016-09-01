6a28878c1ee6f107c8de53a09c87a1fbdca2275c
Letladi <sletladi@gmail.com>
2016-09-01 13:21:39 +0200

    added the business index page

---
+++ spec/controllers/business_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe BusinessController do 
+	describe "GET index" do 
+		it "gets all  the businesses in the database" do 
+			
+		end
+	end
+	describe "GET new"
+	describe "POST create"
+	describe "PATCH update"
+	describe "DELETE destroy"
+end
