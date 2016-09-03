1f04cb4d468bfd2e5096d0ad4d4bc514d498f6f3
Letladi <sletladi@gmail.com>
2016-09-03 16:17:35 +0200

    added a validation so that user cannot review a business more than once

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,4 +6,5 @@ describe Review do
 	it { should validate_presence_of(:rating) }
 	it { should validate_inclusion_of(:rating).in_range(1..5) }
 	it { should validate_presence_of(:description) }
+	it { should validate_uniqueness_of(:user_id).scoped_to(:business_id) }
 end
