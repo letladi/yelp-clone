0a7241ebb6282a600bda1e69609e672022bc6111
Letladi <sletladi@gmail.com>
2016-09-04 11:27:03 +0200

    added 'recent_reviews' method to the review model

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -9,10 +9,27 @@ describe Review do
 	it { should validate_uniqueness_of(:user_id).scoped_to(:business_id) }
 
 	describe 'recent_reviews' do
-		it "returns only 6 reviews if there are more than 6 reviews" do 
-			
+		it "returns an empty array if there are no reviews" do 
+			expect(Review.recent_reviews).to be_empty
 		end
-		it "if there are less than 6 reviews, it returns all of them"
-		it "returns the reviews in descending order by creation date" 
+		it "if there are less than 6 reviews, it returns all of them" do 
+			3.times { Fabricate(:review) }
+			expect(Review.recent_reviews.size).to eq(3)
+		end
+		it "if there are 6 reviews, it returns all of them" do 
+			6.times { Fabricate(:review) }
+			expect(Review.recent_reviews.size).to eq(6)
+		end
+		it "returns only 6 reviews if there are more than 6 reviews" do 
+			8.times { Fabricate(:review) }
+			expect(Review.recent_reviews.size).to eq(6)
+		end
+		
+		
+		it "returns the reviews in descending order by creation date" do 
+			review1 = Fabricate(:review)
+			review2 = Fabricate(:review)
+			expect(Review.recent_reviews).to eq([review2, review1])
+		end
 	end
 end
