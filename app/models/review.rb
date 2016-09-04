75626f4a17938fc79e2246ba1179e910a6b1ed2e
Letladi <sletladi@gmail.com>
2016-09-04 11:27:03 +0200

    added 'recent_reviews' method to the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,8 @@ class Review < ApplicationRecord
 	validates_presence_of :rating, :description
 	validates_inclusion_of :rating, in: (1..5)
 	validates_uniqueness_of :user_id, scope: :business_id
+
+	def self.recent_reviews
+		limit(6).order(created_at: :desc)
+	end
 end
