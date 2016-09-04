204808dc2fe544e02165ad31687dfbfbbb2ff345
Letladi <sletladi@gmail.com>
2016-09-04 18:33:54 +0200

    removed 'add review' link on the businesses link when the user is not logged in

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,9 @@ class Review < ApplicationRecord
 	belongs_to :business
 
 	validates_presence_of :rating, :description
-	validates_inclusion_of :rating, in: (1..5)
+	MIN_RATING = 1
+	MAX_RATING = 5
+	validates_inclusion_of :rating, in: (MIN_RATING..MAX_RATING)
 	validates_uniqueness_of :user_id, scope: :business_id
 
 	LIMIT = 6
